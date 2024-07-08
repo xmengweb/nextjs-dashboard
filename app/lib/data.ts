ccd116dfe032e635cb554861882cb662447849fc
xmengweb <dd2419247021@163.com>
2024-07-08 22:34:04 +0800

    fix: object may be undefined

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -66,10 +66,10 @@ export async function fetchCardData() {
       invoiceStatusPromise,
     ]);
 
-    const numberOfInvoices = Number(data[0].rows[0].count ?? '0');
-    const numberOfCustomers = Number(data[1].rows[0].count ?? '0');
-    const totalPaidInvoices = formatCurrency(data[2].rows[0].paid ?? '0');
-    const totalPendingInvoices = formatCurrency(data[2].rows[0].pending ?? '0');
+    const numberOfInvoices = Number(data[0].rows[0]?.count ?? '0');
+    const numberOfCustomers = Number(data[1].rows[0]?.count ?? '0');
+    const totalPaidInvoices = formatCurrency(data[2].rows[0]?.paid ?? '0');
+    const totalPendingInvoices = formatCurrency(data[2].rows[0]?.pending ?? '0');
 
     return {
       numberOfCustomers,
@@ -132,7 +132,7 @@ export async function fetchInvoicesPages(query: string) {
       invoices.status ILIKE ${`%${query}%`}
   `;
 
-    const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
+    const totalPages = Math.ceil(Number(count.rows[0]?.count) / ITEMS_PER_PAGE);
     return totalPages;
   } catch (error) {
     console.error('Database Error:', error);
